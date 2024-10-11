1a6ca576535c42ed6577ae30437a892978b9e67d
ozerbaykal <baykalozer87@gmail.com>
2024-10-11 08:39:06 +0300

    RestaurantScreen UI fin,DishRow added

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -2,6 +2,8 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { themeColors } from "../theme";
 import * as Icon from "react-native-feather";
+import DishRow from "../components/dishRow";
+import CartIcon from "../components/cartIcon";
 
 const RestaurantScreen = () => {
   const { params } = useRoute();
@@ -10,12 +12,55 @@ const RestaurantScreen = () => {
 
   return (
     <View>
+      <CartIcon />
       <ScrollView>
         <View colassName="relative">
           <Image className="w-full h-72" source={item.image} />
-          <TouchableOpacity className="absolute top-14 left-4 bg-gray-50 rounded-full p-2 shadow">
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            className="absolute top-14 left-4 bg-gray-50 rounded-full p-2 shadow"
+          >
             <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)} />
           </TouchableOpacity>
+        </View>
+
+        <View
+          style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
+          className="bg-white -mt-12 pt-6"
+        >
+          <View className="px-5 ">
+            <Text className="text-3xl font-bold">{item.name}</Text>
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <Image
+                  source={require("../assets/images/fullStar.jpeg")}
+                  className="w-4 h-4"
+                />
+                <Text className="text-xs">
+                  <Text className="text-green-700"> {item.stars}</Text>
+                  <Text className="text-gray-700">
+                    ({item.reviews}review) •
+                    <Text className=" font-semibold"> {item.category}</Text>
+                  </Text>
+                </Text>
+              </View>
+              <View className="flex-row items-center space-x-1">
+                <Icon.MapPin color="gray" width="15" height="15" />
+                <Text className="text-gray-700 text-sm">
+                  {" "}
+                  Nearby • {item.address}
+                </Text>
+              </View>
+            </View>
+            <Text className="text-gray-500 mt-2"> {item.description}</Text>
+          </View>
+        </View>
+        <View className="pb-36 bg-white">
+          <Text className="px-4 py-4 text-2xl font-bold ">Menu</Text>
+          {/* dishes */}
+          {item.dishes.map((dish, index) => (
+            <DishRow item={{ ...dish }} key={index} />
+          ))}
         </View>
       </ScrollView>
     </View>
